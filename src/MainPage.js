d10c1a6a4ce9ef69081ee9d307966682efc1a4b2
zsj <cho010105@naver.com>
2024-08-10 15:11:24 +0900

    [ADD] 유실물 컬럼 클릭 시 해당 마커 이동 구현

--- src/MainPage.js
+++ src/MainPage.js
@@ -116,51 +116,62 @@ import React, { useEffect, useRef, useState } from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 import { columns } from "./columns";
+import {Drawer} from "@mui/material";
 
 const MainPage = () => {
     const mapElement = useRef(null);
     const { naver } = window;
 
     const rows = [
-        { id: 1, object: "따릉이", latitude: "37.548769", longitude: "127.120038" },
-        { id: 2, object: "따릉이2", latitude: "37.521302", longitude: "126.984974" },
-        { id: 3, object: "따릉이3", latitude: "37.552973", longitude: "126.984190" },
-        { id: 4, object: "따릉이4", latitude: "37.493379", longitude: "126.919990" },
-        { id: 5, object: "따릉이5", latitude: "37.564087", longitude: "126.891791" },
-        { id: 6, object: "따릉이6", latitude: "37.526674", longitude: "126.934719" }
+        { id: 1, object: "따릉이", latitude: "37.548769", longitude: "127.120038", address: "서울특별시 강동구 선사로 83-66" },
+        { id: 2, object: "따릉이2", latitude: "37.521302", longitude: "126.984974", address: "서울특별시 용산구 서빙고로 185" },
+        { id: 3, object: "따릉이3", latitude: "37.552973", longitude: "126.984190", address: "서울특별시 용산구 용산동2가" },
+        { id: 4, object: "따릉이4", latitude: "37.493379", longitude: "126.919990", address: "서울특별시 동작구 여의대방로20길 33" },
+        { id: 5, object: "따릉이5", latitude: "37.564087", longitude: "126.891791", address: "서울특별시 마포구 상암동 482" },
+        { id: 6, object: "따릉이6", latitude: "37.526674", longitude: "126.934719", address: "서울특별시 영등포구 여의동로 330" }
     ];
+
+    const [map, setMap] = useState(null);
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const [selectedRow, setSelectedRow] = useState(null);
+
+    // const [myLocation, setMyLocation] = useState({
+    //     latitude: 37.4979517, // 초기값 = 강남역 좌표
+    //     longitude: 127.0276188,
+    // });
 
     const [myLocation, setMyLocation] = useState({
         latitude: 37.4979517, // 초기값 = 강남역 좌표
         longitude: 127.0276188,
     });
 
-    // useEffect(() => {   // 현재 위치 자동 업데이트 기능 --> 굳이 필요는 없을듯
-    //     const updateLocation = () => {
-    //         if (navigator.geolocation) {
-    //             navigator.geolocation.getCurrentPosition(
-    //                 (position) => {
-    //                     setMyLocation({
-    //                         latitude: position.coords.latitude,
-    //                         longitude: position.coords.longitude,
-    //                     });
-    //                 },
-    //                 () => {
-    //                     // 오류 발생 시 기본 위치 설정 (강남역)
-    //                     setMyLocation({
-    //                         latitude: 37.4979517,
-    //                         longitude: 127.0276188,
-    //                     });
-    //                 }
-    //             );
-    //         }
-    //     };
-    //     updateLocation();   // 페이지 로드마다 현재 위치를 갱신
-    //
-    //     const intervalId = setInterval(updateLocation, 60000);  // 1분마다 내 위치를 업데이트할 수 있도록 setInterval 사용
-    //
-    //     return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 인터벌 제거
-    // }, []);
+    useEffect(() => {
+        const updateLocation = () => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        setMyLocation({
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude,
+                        });
+                    },
+                    () => {
+                        // 오류 발생 시 기본 위치 설정 (강남역)
+                        setMyLocation({
+                            latitude: 37.4979517,
+                            longitude: 127.0276188,
+                        });
+                    }
+                );
+            }
+        };
+
+        updateLocation();   // 페이지 로드마다 현재 위치를 갱신
+
+        const intervalId = setInterval(updateLocation, 60000);  // 1분마다 내 위치를 업데이트할 수 있도록 setInterval 사용
+
+        return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 인터벌 제거
+    }, []);
 
     const seoulCenter = {   // 서울 중심을 기준으로 마커들을 표시하기 위해 생성함.
         latitude: 37.5505,
@@ -173,22 +184,36 @@ const MainPage = () => {
         const location = new naver.maps.LatLng(seoulCenter.latitude, seoulCenter.longitude);
         const mapOptions = {
             center: location,
-            zoom: 12,  // 서울시 전체를 볼 수 있는 줌 레벨 설정
+            zoom: 12,
             zoomControl: true,
         };
 
-        const map = new naver.maps.Map(mapElement.current, mapOptions);
+        const newMap = new naver.maps.Map(mapElement.current, mapOptions);
 
-        // 각 row에 대해 Marker를 생성하여 지도에 추가합니다.
+
         rows.forEach((row) => {
             new naver.maps.Marker({
                 position: new naver.maps.LatLng(parseFloat(row.latitude), parseFloat(row.longitude)),
-                map: map,
+                map: newMap,
                 title: row.object,
             });
         });
 
-    }, [myLocation, rows]);
+        setMap(newMap);
+    }, [naver]);
+
+    const handleObjectClick = (params) => {
+        const row = rows.find(r => r.id === params.id);
+        if (row) {
+            const location = new naver.maps.LatLng(parseFloat(row.latitude), parseFloat(row.longitude));
+            map.setCenter(location);
+            map.setZoom(20);  // 확대 수준 조절
+
+            // 드로어에 표시할 정보 설정
+            setSelectedRow(row);
+            setDrawerOpen(true);
+        }
+    };
 
     return (
         <React.Fragment>
@@ -201,25 +226,38 @@ const MainPage = () => {
                     <option>보라매공원</option>
                     <option>남산공원</option>
                 </select>
-
             </div>
             <div ref={mapElement} className='map-naver-view'></div>
             <Box>
                 <DataGrid
                     rows={rows} //위 rows의 임의 데이터를 데이터그리드에 적용.
                     columns={columns}
+                    onCellClick={handleObjectClick}
                     initialState={{
-                        pagination: {
-                            paginationModel: {
-                                pageSize: 5
-                            },
-                        },
+                        // pagination: {
+                        //     paginationModel: {
+                        //         pageSize: 5
+                        //     },
+                        // },   // 모든 rows를 한 페이지 안에 모두 보이게. 대신 DataGrid 컴포넌트 구성상 최대 100개까지만 보일 수 있음.
                     }}
-                    pageSizeOptions={[5]}
+                    // pageSizeOptions={[5]}
                     checkboxSelection
                     disableRowSelectionOnClick
                 />
             </Box>
+            <Drawer
+                anchor='left'
+                open={drawerOpen}
+                onClose={() => setDrawerOpen(false)}
+            >
+                {selectedRow && (
+                    <Box p={2} width="250px" textAlign="center">
+                        <h2>{selectedRow.object}</h2>
+                        <img src={`https://via.placeholder.com/200`} alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
+                        <p>{selectedRow.address}</p>
+                    </Box>
+                )}
+            </Drawer>
         </React.Fragment>
     );
 };
