10709ca3977ff0af6450ef175613408d8ff55cde
zsj <cho010105@naver.com>
2024-10-01 15:19:20 +0900

    [Change] 라우팅 방식 HashRouter로 변경

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Selected_park from "./selected_park";
 import MainPage_park from "./mainPage_park";
 
 const Router = () => {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<MainPage_park />} /> {/* 첫 시작 페이지 */}
                 <Route path="/park/:parkName" element={<Selected_park />} /> {/* MainPage의 리스트에서 공원 이름 클릭 시 */}
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
